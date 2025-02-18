3224fde2fa1fddce4d67fbb6fc2faa028ea48dd5
sainithish <venkata.gsnc@gmail.com>
2025-02-17 19:32:01 -0800

    Python commits Feb17

--- src/print_formatting.py
+++ src/print_formatting.py
@@ -9,6 +9,8 @@ def main():
     print('printing a floating point number %25.10f' % s2)
     print('Convert an object to string %s' % 123456)
     print('Convert an object to string %s' % 12.3456)
+    print('This is first %s and second %s' % ('Sai', 'Nithish'))
+
 
 
 if __name__ == "__main__":
