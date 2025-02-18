29da119e5b75a4c1fd0a9297234498b4ffdb2910
sainithish <venkata.gsnc@gmail.com>
2025-02-17 19:34:12 -0800

    Python commits Feb17-2

---
+++ src/print_formatting2.py
@@ -0,0 +1,7 @@
+def main():
+    print('This is first %s and second %s' % ('Sai', 'Nithish'))
+    print('First: {x} and second: {y} and third: {x}'.format(x='inserted', y='second inserted'))
+
+
+if __name__ == "__main__":
+    main()
